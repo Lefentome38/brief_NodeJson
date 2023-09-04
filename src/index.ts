cf4006bcd75430214ae1c77ad63f9beebcff94c9
aymeric <rambaudaymeric@gmail.com>
2023-09-04 15:55:14 +0200

    argument non metrise

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello le monde");
+console.log(process.argv);
+
+Argu()
+
+function Argu() {
+    if (process.argv.indexOf('--help') != -1) {
+        console.log('je vais vous aider');     
+    }
+    else if (process.argv.indexOf('--name') != -1) {
+        console.log('Bonjour ' + process.argv[process.argv.indexOf('--name') + 1] );
+    }
+    else {
+        console.log("hello le monde");
+    }
+}
+
+// function Argu() {
+//     arguments[0] === "tata";
+//     arguments[1] === "gg";
+//     arguments[2] === "help";
+//     console.log(arguments[1]);
+//     console.log(process.argv.length);
+// }
